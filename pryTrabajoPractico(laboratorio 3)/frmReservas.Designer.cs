539aefd53a4b955003d3a14d97b35e85efbcbd98
root <mgiraudo@ies21.edu.ar>
2024-05-22 13:04:13 -0300

    Agregar archivos de proyecto.

---
+++ pryTrabajoPractico(laboratorio 3)/frmReservas.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace pryTrabajoPractico_laboratorio_3_
+{
+    partial class frmReservas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboTeatros = new System.Windows.Forms.ComboBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReservar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboTeatros
+            // 
+            this.cboTeatros.FormattingEnabled = true;
+            this.cboTeatros.Items.AddRange(new object[] {
+            "Quenaken",
+            "Onas",
+            "Tobas"});
+            this.cboTeatros.Location = new System.Drawing.Point(12, 38);
+            this.cboTeatros.Name = "cboTeatros";
+            this.cboTeatros.Size = new System.Drawing.Size(119, 23);
+            this.cboTeatros.TabIndex = 0;
+            this.cboTeatros.SelectedIndexChanged += new System.EventHandler(this.cboTeatros_SelectedIndexChanged);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(12, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(112, 15);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Seleccione el teatro:";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(12, 94);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(119, 23);
+            this.dtpFecha.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(12, 151);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(119, 23);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Fecha:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre y Apellido:";
+            // 
+            // btnReservar
+            // 
+            this.btnReservar.Location = new System.Drawing.Point(12, 192);
+            this.btnReservar.Name = "btnReservar";
+            this.btnReservar.Size = new System.Drawing.Size(119, 23);
+            this.btnReservar.TabIndex = 6;
+            this.btnReservar.Text = "Reservar";
+            this.btnReservar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(12, 221);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(119, 23);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmReservas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(496, 425);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnReservar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.cboTeatros);
+            this.Name = "frmReservas";
+            this.Text = "frmReservas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox cboTeatros;
+        private Label lblTitulo;
+        private DateTimePicker dtpFecha;
+        private TextBox txtNombre;
+        private Label label1;
+        private Label label2;
+        private Button btnReservar;
+        private Button btnCancelar;
+    }
+}
---
+++ pryTrabajoPractico(laboratorio 3)/frmReservas.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryTrabajoPractico_laboratorio_3_
+{
+    public partial class frmReservas : Form
+    {
+        public frmReservas()
+        {
+            InitializeComponent();
+        }
+
+        int asientos(int cantidadAsientos, int asientosFila, Color color, GroupBox group, int xInicial, int yInicial)
+        {
+            PictureBox asiento;
+            int x = xInicial;
+            int y = yInicial;
+            asiento = new PictureBox();
+            asiento.Size = new Size(75,20);
+            asiento.Location = new Point(25,10);
+            asiento.BackColor = Color.Brown;
+            group.Controls.Add(asiento);
+            
+            for (int i = 0; i < cantidadAsientos / asientosFila; i++)
+            {
+                for (int z = 0; z < asientosFila; z++)
+                {
+                    asiento = new PictureBox();
+                    asiento.Size = new Size(10, 10);
+                    asiento.Location = new Point(x, y);
+                    asiento.BackColor = color;
+                    group.Controls.Add(asiento);
+                    x += 20;
+                }
+                x = 20;
+                y += 20;
+                group.Size = new Size(group.Size.Width, group.Size.Height + 10);
+            }
+            return y;
+          
+        }
+
+        private void cboTeatros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GroupBox group = new GroupBox();
+
+            if (cboTeatros.SelectedIndex == 0)
+            {
+                this.Controls.Remove(group);
+                group.Name = "Group";
+                group.Location = new Point(160, 35);
+                group.Size = new Size(130, 250);
+                int y = asientos(20, 5, Color.Blue, group, 20, 80);
+                asientos(20, 5, Color.Green, group, 20, y + 20);
+                this.Controls.Add(group);
+            }
+            else if (cboTeatros.SelectedIndex == 1)
+            {
+                group = new GroupBox();
+                group.Location = new Point(160, 35);
+                group.Size = new Size(130, 250);
+                int y = asientos(40, 4, Color.Blue, group, 20, 80);
+                asientos(20, 5, Color.Green, group, 20, y + 20);
+                this.Controls.Add(group);
+            }
+            else
+            {
+                group = new GroupBox();
+                group.Location = new Point(160, 35);
+                group.Size = new Size(130, 250);
+                int y = asientos(10, 3, Color.Blue, group, 20, 80);
+                asientos(15, 5, Color.Green, group, 20, y + 20);
+                this.Controls.Add(group);
+            }
+        }
+    }
+}
